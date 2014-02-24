c8c74edd281c02d2e2609b7e3f478d114303e6e4
jarllarsson <jarl.larsson@gmail.com>
2014-02-24 20:00:02 +0100

    can position object in prototype kd-tree as long as all axes are positive. Needs to be fixed!

--- Protos/kd-tree-test/Assets/Node.cs
+++ Protos/kd-tree-test/Assets/Node.cs
@@ -2,12 +2,91 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
+public struct AxisMark
+{
+    public enum AXIS
+    {
+        X,Y,Z
+    }
+    // b_2 b_1
+    // 00 - x
+    // 01 - y
+    // 10 - z
+    public bool b_1;
+    public bool b_2;
+
+    public void setVec(AXIS p_axis)
+    {
+        setVec((int)p_axis);
+    }
+
+    public void setVec(int p_xyz)
+    {
+        if (p_xyz == 0)
+            setVec(true, false, false);
+        else if (p_xyz == 1)
+            setVec(false, true, false);
+        else
+            setVec(false, false, true);
+    }
+
+    public void setVec(bool p_x, bool p_y, bool p_z)
+    {
+        if (p_x)
+        {
+            b_1 = false;
+            b_2 = false;
+        }
+        else if (p_y)
+        {
+            b_1 = true;
+            b_2 = false;
+        }
+        else
+        {
+            b_1 = false;
+            b_2 = true;
+        }
+    }
+
+    public Vector3 getVec()
+    {
+        Vector3 reval = Vector3.zero;
+        switch (b_1)
+        {
+            case false:
+                {
+                    switch (b_2)
+                    {
+                        case false:
+                            {
+                                reval = Vector3.right;
+                                break;
+                            }
+                        case true:
+                            {
+                                reval = Vector3.forward;
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case true:
+                {
+                    reval = Vector3.up;
+                    break;
+                }
+        }
+        return reval;
+    }
+}
+[System.Serializable]
 public class Node
 {
-    public Vector3 m_split = new Vector3();
+    public AxisMark m_split;
     public bool m_isLeaf = false;
     public float m_position = 0.0f;
-    public int m_leftChildIdx = 0;
-    public int m_rightChildIdx = 0;
+    public int m_leftChildIdx = 0; // right child is always left child+1
     public List<object> m_objects = new List<object>();
 }
--- Protos/kd-tree-test/Assets/kdTree.cs
+++ Protos/kd-tree-test/Assets/kdTree.cs
@@ -4,74 +4,144 @@ using System.Collections.Generic;
 
 public class kdTree : MonoBehaviour 
 {
-    public List<Node> m_tree = new List<Node>();
+    public List<GameObject> m_objects;
+    public Node[] m_tree;
     public Vector3 m_box;
     public float m_firstSplitDist = 0.0f;
 	// Use this for initialization
 	void Start () 
     {
-        Node tree = new Node();
-        tree.m_split = Vector3.forward;
-        tree.m_isLeaf = false;
-        tree.m_position = 1.0f;
-        tree.m_leftChildIdx = 1; // upcoming
-        tree.m_rightChildIdx = 2;// upcoming
-        m_tree.Add(tree);
+        Random.seed=(int)(Time.time*1000.0f);
+        m_tree = new Node[256]; // pre-allocate
 
+        Node root = new Node();
+        foreach (GameObject obj in m_objects)
+        {
+            for (int i=0;i<3;i++)
+            {
+                float absa=2.0f*Mathf.Abs(obj.transform.position[i]-transform.position[i]);
+                Debug.Log(absa);
+                if (absa>m_box[i])
+                   m_box[i]=absa;
+            }
+            root.m_objects.Add(obj as object);
+        }
+        m_tree[1] = root;
+        buildTree(root, 0, 0, 1, Vector3.zero); // start at 1
+
+        /*
         tree = new Node();
-        tree.m_split = Vector3.up;
+        tree.m_split.setVec(AxisMark.AXIS.Y);
         tree.m_isLeaf = false;
         tree.m_position = 0.2f;
         tree.m_leftChildIdx = 3;
-        tree.m_rightChildIdx = 4;
         m_tree.Add(tree);
 
         tree = new Node();
-        tree.m_split = Vector3.up;
+        tree.m_split.setVec(AxisMark.AXIS.Y);
         tree.m_isLeaf = true;
         tree.m_position = -0.2f;
         tree.m_leftChildIdx = 0;
-        tree.m_rightChildIdx = 0;
         m_tree.Add(tree);
 
         tree = new Node();
-        tree.m_split = Vector3.right;
+        tree.m_split.setVec(AxisMark.AXIS.X);
         tree.m_isLeaf = false;
         tree.m_position = 0.2f;
         tree.m_leftChildIdx = 5;
-        tree.m_rightChildIdx = 6;
         m_tree.Add(tree);
 
         tree = new Node();
-        tree.m_split = Vector3.right;
+        tree.m_split.setVec(AxisMark.AXIS.X);
         tree.m_isLeaf = true;
         tree.m_position = -0.2f;
         tree.m_leftChildIdx = 0;
-        tree.m_rightChildIdx = 0;
         m_tree.Add(tree);
 
         tree = new Node();
-        tree.m_split = Vector3.forward;
+        tree.m_split.setVec(AxisMark.AXIS.Z);
         tree.m_isLeaf = true;
         tree.m_position = 0.1f;
         tree.m_leftChildIdx = 0;
-        tree.m_rightChildIdx = 0;
         m_tree.Add(tree);
 
         tree = new Node();
-        tree.m_split = Vector3.forward;
+        tree.m_split.setVec(AxisMark.AXIS.Z);
         tree.m_isLeaf = true;
         tree.m_position = -0.3f;
         tree.m_leftChildIdx = 0;
-        tree.m_rightChildIdx = 0;
         m_tree.Add(tree);
+         * */
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        m_tree[0].m_position = m_firstSplitDist;
+        //m_tree[1].m_position = m_firstSplitDist;
 	}
+
+    void buildTree(Node p_node, int p_dimsz, int p_dim,  int p_idx, Vector3 p_accDist)
+    {
+
+        if (p_dimsz > 6 || p_node.m_objects.Count < 1 || p_idx<<1>m_tree.Length-2) 
+        {
+            p_node.m_isLeaf = true;
+            return;
+        }
+        Debug.Log("dim: "+p_dim);
+        if (p_dim > 2) p_dim = 0;
+        
+        AxisMark splitPlane=new AxisMark();
+        splitPlane.setVec(p_dim);
+	    float splitpos = findOptimalSplitPos(p_node);
+	    Node leftnode = new Node();
+        Node rightnode = new Node(); 
+        p_node.m_leftChildIdx = p_idx << 1;
+        p_node.m_split = splitPlane;
+        p_node.m_position = splitpos;
+        //
+        m_tree[p_node.m_leftChildIdx] = leftnode;
+        m_tree[p_node.m_leftChildIdx+1] = rightnode;        
+        //
+	    foreach (object obj in p_node.m_objects)
+	    {
+            if (objIntersectNode(true, splitpos, p_accDist, splitPlane, obj)) 
+            {
+                //p_node.m_objects.Remove(obj);
+                leftnode.m_objects.Add(obj);
+                Debug.Log("left");
+            }
+            if (objIntersectNode(false, splitpos, p_accDist, splitPlane, obj))
+            {
+                //p_node.m_objects.Remove(obj);
+                rightnode.m_objects.Add(obj);
+                Debug.Log("right");
+            }
+	    }
+        buildTree(leftnode, p_dimsz + 1, p_dim + 1, p_node.m_leftChildIdx, p_accDist + splitpos * splitPlane.getVec()); // power of two structure
+        buildTree(rightnode, p_dimsz + 1, p_dim + 1, p_node.m_leftChildIdx + 1, p_accDist + splitpos * splitPlane.getVec());
+    }
+
+    bool objIntersectNode(bool p_isLeft, float p_splitpos, Vector3 p_accDist, AxisMark p_axis, object p_obj)
+    {
+        GameObject gobj = p_obj as GameObject;
+        Vector3 axis = p_axis.getVec();
+        //float val = Vector3.Dot(Vector3.Normalize(gobj.transform.position - transform.position - p_axis.getVec() * p_splitpos), axis);
+        Vector3 scaled = entrywiseMul(axis,p_accDist);
+        Vector3 objlen = entrywiseMul(axis,gobj.transform.position-transform.position);
+        float val = (scaled.x + scaled.y + scaled.z);
+        float valcomp = (objlen.x + objlen.y + objlen.z);
+        Debug.Log("val"+val);
+        Debug.Log("valcmp" + valcomp);
+        if ((p_isLeft && val < valcomp ) || (!p_isLeft && val>valcomp))
+            return true;
+        return false;
+    }
+
+    float findOptimalSplitPos(Node p_node)
+    {
+        return 0.1f;
+    }
 
     void OnDrawGizmos()
     {
@@ -79,34 +149,47 @@ public class kdTree : MonoBehaviour
 
         Vector3 parentOrigo = transform.position;
 
-        if (m_tree.Count>0)
-            drawNode(parentOrigo, m_box,0);
+        if (m_tree!=null && m_tree.Length>0)
+            drawNode(parentOrigo, m_box,1);
     }
 
     void drawNode(Vector3 pos, Vector3 parentSize,int idx)
     {
-
         // offset to get split plane
-        Vector3 split=m_tree[idx].m_split;
-        Vector3 offset=split * m_tree[idx].m_position;
-        Vector3 currentOrigo = pos + offset;
-        Gizmos.color = new Color(split.x,split.y,(float)idx/(float)m_tree.Count);
-
-        // draw split plane
-        Vector3 drawSize=parentSize-new Vector3(parentSize.x*split.x,parentSize.y*split.y,parentSize.z*split.z);
-        Gizmos.DrawWireCube(currentOrigo,drawSize);
-
-        if (!m_tree[idx].m_isLeaf)
+        if (idx < m_tree.Length && m_tree[idx]!=null)
         {
-            //Vector3 dir = new Vector3(0.0f, vertSplit, horzSplit);
-            Vector3 splitH = 0.5f * split;
-            Vector3 lsize = (parentSize-offset*2.0f);
-            lsize=new Vector3(lsize.x-lsize.x*splitH.x, lsize.y-lsize.y*splitH.y, lsize.z-lsize.z*splitH.z);
-            Vector3 rsize = (-parentSize-offset*2.0f);
-            rsize=new Vector3(rsize.x-rsize.x*splitH.x, rsize.y-rsize.y*splitH.y, rsize.z-rsize.z*splitH.z);
-            drawNode(currentOrigo+0.5f*new Vector3(lsize.x*split.x,lsize.y*split.y,lsize.z*split.z), lsize, m_tree[idx].m_leftChildIdx);
-            drawNode(currentOrigo+0.5f*new Vector3(rsize.x*split.x,rsize.y*split.y,rsize.z*split.z), rsize, m_tree[idx].m_rightChildIdx);
+            Vector3 split=m_tree[idx].m_split.getVec();
+            Vector3 offset=split * m_tree[idx].m_position;
+            Vector3 currentOrigo = pos + offset;
+            Gizmos.color = new Color(0.5f,0.5f,0.5f)+new Color(split.x,split.y,(float)idx/(float)m_tree.Length);
+
+            // draw split plane
+            Vector3 drawSize=parentSize-entrywiseMul(parentSize, split);
+            Gizmos.DrawWireCube(currentOrigo,drawSize);
+
+            foreach (object obj in m_tree[idx].m_objects)
+            {
+                Gizmos.DrawWireSphere(((GameObject)obj).transform.position, 0.1f);
+            }
+
+            if (!m_tree[idx].m_isLeaf)
+            {
+                //Vector3 dir = new Vector3(0.0f, vertSplit, horzSplit);
+                Vector3 splitH = 0.5f * split;
+                Vector3 lsize = (parentSize-offset*2.0f);
+                lsize=lsize-entrywiseMul(lsize, splitH);
+                Vector3 rsize = (-parentSize-offset*2.0f);
+                rsize=rsize-entrywiseMul(rsize, splitH);
+                drawNode(currentOrigo+0.5f*entrywiseMul(lsize, split), lsize, m_tree[idx].m_leftChildIdx);
+                drawNode(currentOrigo+0.5f*entrywiseMul(rsize, split), rsize, m_tree[idx].m_leftChildIdx+1);
+            }
         }
+
         return;
+    }
+
+    Vector3 entrywiseMul(Vector3 p_a, Vector3 p_b)
+    {
+        return new Vector3(p_a.x * p_b.x, p_a.y * p_b.y, p_a.z * p_b.z);
     }
 }
